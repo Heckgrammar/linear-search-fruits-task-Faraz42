6a4d95da6f1ad6cabc80fba516d28e0ef8ea19d4
Faraz Shariff <21FShariff@heckgrammar.co.uk>
2025-02-01 14:13:16 +0000

    finished homework

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -5,14 +5,24 @@
         static void Main(string[] args)
         {
             string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
+            Console.WriteLine("what fruit would you like to find");
+            string FruitToFind = Console.ReadLine();
+            bool found = false;
+            for (int i = 0; i < fruits.Length; i++) 
+            {
+                if (fruits[i] == FruitToFind) 
+                {
+                    found = true;
+                    Console.WriteLine(found);
+                    break;
 
-            // Your program should:
-            // Ask the user what word they would like to find
-            // Output the message True if the word is found
-            // Output the message False if the word is not found
-            
-            // You must write your own linear search routine and not use any built-in fuctions available in C#
-            // You should use meaningful variable names
+                }
+                else if (i == fruits.Length - 1) 
+                {
+                    Console.WriteLine(found);
+                    break;
+                } 
+            }
         }
     }
 }
